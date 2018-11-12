333a3da25c9931e945ec0c139d033190c6125cd9
Cory Billington <mcorybillington@gmail.com>
2018-11-12 00:27:25 -0500

    Displayed current password being tried, moved to subprocess, moved args to separ ate function, fixed PEP violations, and checked to see if steghide was installed

--- steg_brute.py
+++ steg_brute.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-
+from subprocess import Popen, PIPE, DEVNULL
 from progressbar import ProgressBar, Percentage, Bar
 from argparse import ArgumentParser
-import commands
 import os
+import sys
 
 
-class color:
+class Color:
     FAIL = '\033[91m'
     BLUE = '\033[94m'
     BLUE2 = '\033[1;36m'
     INFO = '\033[93m'
     ENDC = '\033[0m'
     GREEN = '\033[1;32m'
-
-    
-def check_file(file):
-    if os.path.exists(file):
-        return True
-    else:
-        return False
 
     
 VERSION = "1.0"
@@ -43,18 +36,47 @@ Command line examples:
     """
 
 
-def Steg_brute(ifile, dicc):
+def check_file(file):
+    return os.path.exists(file)
+
+
+def check_steghide():
+    try:
+        Popen(['steghide', '--help'], stdout=DEVNULL, stderr=DEVNULL)
+    except FileNotFoundError:
+        print("Is steghide installed?")
+        print("     sudo apt-get install steghide")
+        print("     sudo yum install steghide")
+        print("     sudo dnf install steghide")
+        sys.exit()
+
+
+def count_lines(fname):
+    i = 1
+    with open(fname, 'rb') as f:
+        for i, l in enumerate(f):
+            pass
+    return i
+
+
+def steg_brute(ifile, dicc):
     i = 0
     ofile = ifile.split('.')[0] + "_flag.txt"
-    nlines = len(open(dicc).readlines())
+    nlines = count_lines(dicc)
     with open(dicc, 'r') as passFile:
         pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=nlines).start()
-        for line in passFile.readlines():
-            password = line.strip('\n')
-            r = commands.getoutput("steghide extract -sf %s -p '%s' -xf %s" % (ifile, password, ofile))
-            if not "no pude extraer" in r and not "could not extract" in r:
-                print(color.GREEN + "\n\n " + r + color.ENDC)
-                print("\n\n [+] " + color.INFO + "Information obtained with password:" + color.GREEN + " %s\n" % password + color.ENDC)
+        print('\n')
+        for pass_line in passFile:
+            password = pass_line.strip('\n')
+            r = Popen(['steghide', 'extract', '-sf', ifile, '-p', password, '-xf', ofile], stdout=PIPE, stderr=PIPE)
+            output = r.communicate()[1].decode('utf-8')
+            sys.stdout.flush()
+            sys.stdout.write('\r')
+            sys.stdout.write("Trying: {}".format(password))
+            if "no pude extraer" not in output and "could not extract" not in output:
+                print(Color.GREEN + "\n " + output + Color.ENDC)
+                print("\n\n [+] " + Color.INFO +
+                      "Information obtained with password:" + Color.GREEN + " {}\n".format(password + Color.ENDC))
                 if check_file(ofile):
                     with open(ofile, 'r') as outfile:
                         for line in outfile.readlines():
@@ -66,23 +88,21 @@ def Steg_brute(ifile, dicc):
 
 def steghide(ifile, passwd):
     ofile = ifile.split('.')[0] + "_flag.txt"
-    r = commands.getoutput("steghide extract -sf %s -p '%s' -xf %s" % (ifile, passwd, ofile))
-    if not "no pude extraer" in r and not "could not extract" in r:
-        print(color.GREEN + "\n\n " + r + color.ENDC)
-        print("\n [+] " + color.INFO + "Information obtained: \n" + color.ENDC)
+    r = Popen(['steghide', 'extract', '-sf', ifile, '-p', passwd, '-xf', ofile], stdout=PIPE, stderr=PIPE)
+    output = r.communicate()[1].decode('utf-8')
+    if "no pude extraer" not in output and "could not extract" not in output:
+        print(Color.GREEN + "\n\n " + output + Color.ENDC)
+        print("\n [+] " + Color.INFO + "Information obtained: \n" + Color.ENDC)
         if check_file(ofile):
             with open(ofile, 'r') as myfile:
                 for line in myfile.readlines():
                     print(line)
     else:
-        print(color.FAIL + "\n\n " + r + color.ENDC)
+        print(Color.FAIL + "\n\n " + output + Color.ENDC)
 
 
-def main():
-    argp = ArgumentParser(
-            description="Steghide Brute Force Tool",
-            usage="./steg_brute.py [options] [-f image_file]",
-            version="Steghide Brute Force Tool v" + VERSION)
+def arguments():
+    argp = ArgumentParser(description="Steghide Brute Force Tool v" + VERSION)
 
     argp.add_argument('-i', '--info', dest='info', action='store_true',
                       help='Get info of file')
@@ -102,18 +122,26 @@ def main():
     argp.add_argument('-d', '--dictionary', dest='dicc',
                       help='Path of dictionary to brute force attack')
 
-    args = argp.parse_args()
-    #print vars(args)
+    return argp.parse_args()
+
+
+def main():
+
+    check_steghide()
+    args = arguments()
 
     if args.info and not args.extract and not args.brute:
-        os.system("steghide info %s" % (args.file))
+        os.system("steghide info {}".format(args.file))
 
     elif args.extract and not args.info and not args.brute:
         steghide(args.file, args.password)
 
     elif args.brute and not args.info and not args.extract:
-        print("\n [i] " + color.INFO + "Searching..." + color.ENDC)
-        Steg_brute(args.file, args.dicc)
+        print("\n [i] " + Color.INFO + "Searching..." + Color.ENDC)
+        if not check_file(args.dicc):
+            print("No dictionary file found at {}".format(args.dicc))
+        else:
+            steg_brute(args.file, args.dicc)
 
     else:
         print(SAMPLES)
